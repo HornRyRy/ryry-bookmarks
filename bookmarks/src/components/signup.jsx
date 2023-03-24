05e84bf59ac39e13a4e8ca5daeb5936765e0466e
Ryan Horn <rj.horn@gmail.com>
2023-03-23 18:21:43 -0600

    signup additions

--- bookmarks/src/components/signup.jsx
+++ bookmarks/src/components/signup.jsx
@@ -1,11 +1,53 @@
 import React from "react";
+import { useState } from "react";
 
 
+function Signup({}) {
 
-function Signup() {
+    const [nickname, setNickname] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleSubmit(e){
+        e.preventDefault()
+        const user ={
+            nickname, 
+            email,
+            password
+        }
+        fetch('/users', {
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            body:JSON.stringify(user)
+        })
+        .then(resp => {
+            if(resp.status === 201){
+                resp.json().then(user => setUser(user))
+                console.log("Users post status 201")
+            }else{
+                resp.json().then(console.log("no 201 status"))
+            }
+        })
+    }
+
+
   return (
-    <div>
-        <p>this is where the signup goes</p>
+    <div>            
+    <form onSubmit= {handleSubmit}>
+        <h2>Signup Here</h2>
+        <div></div>
+        <label>Name </label>
+        <input placeholder="App will call you by this" type='text' name='name' value={nickname} onChange={e => setName(e.target.value)} />
+        <div></div>
+        <label>Username </label>
+        <input placeholder="Username (email)" type='text' name='email' value={email} onChange={e => setUsername(e.target.value)} />
+        <div></div>
+        <label>Password </label>
+        <input placeholder="Password" type='password' name='password' value={password} onChange={e => setPassword(e.target.value)} />
+        <div></div>
+        <input type='submit' value='Register' />
+    </form>
+        
     </div>
   )
 }
