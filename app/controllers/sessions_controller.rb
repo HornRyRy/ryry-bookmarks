b3b0bd2cb00e2ce84e27788585b2ae288e416f81
Ryan Horn <rj.horn@gmail.com>
2023-03-24 11:10:38 -0600

    sessions controller work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,12 @@
 class SessionsController < ApplicationController
-    
+
+    def create
+        user = User.find_by(name:params[:name])
+        if user && user.authenticate(params[:password])
+            render json:user, status: :ok
+        else
+            render json: {error: "Invalid Username and/or Password"}, status: :unauthorized
+        end
+    end
+
 end
