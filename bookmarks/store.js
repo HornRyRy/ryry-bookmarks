c5919a8e1483f753485a98129d1bd8a6776cf793
Ryan Horn <rj.horn@gmail.com>
2023-03-26 17:42:44 -0600

    more redux auth

--- bookmarks/store.js
+++ bookmarks/store.js
@@ -1,5 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { curryGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware'
+import { userApi } from './src/services/userApi'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [userApi.reducerPath]: userApi.reducer
+  },
+  middleware: (curryGetDefaultMiddleware) =>{
+    return curryGetDefaultMiddleware().concat(userApi.middleware)
+  }
 })
