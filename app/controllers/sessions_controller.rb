aec528c2ffbc8cb115b4dcbd76b746a95fd73b69
Ryan Horn <rj.horn@gmail.com>
2023-03-28 12:37:51 -0600

    Login Logout fixes w/ Lantz

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
     def create
-        user = User.find_by(name:params[:name])
+        user = User.find_by(email:params[:email])
         if user && user.authenticate(params[:password])
             render json:user, status: :ok
         else
