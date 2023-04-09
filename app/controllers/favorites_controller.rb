066c47a161a56fc47bbff991e1913bb4dafbe5e1
Ryan Horn <rj.horn@gmail.com>
2023-04-08 21:19:47 -0600

    favorite controller changes

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,32 @@
 class FavoritesController < ApplicationController
+
+
+    #link up your user id categories
+
+    def index
+        favorites = Favorite.all
+        render json: favorites, status: :ok
+    end
+
+    def create
+        favorite = Favorite.create!(favorite_params)
+        render json: favorite, status: :created
+    end
+
+    def update
+        @favorite.update(favorite_params)
+        render json: @favorite, status: :accepted
+    end
+
+    def destroy
+        @favorite.destroy
+        head :no_content
+    end
+
+    private
+
+    def favorite_params
+        params.permit(:url, :user_id, :gallery_id)
+    end
+
 end
